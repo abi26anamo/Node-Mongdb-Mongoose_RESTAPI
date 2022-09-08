17895e561a47c25a2524981b43123ee05bbbdc52
abi26anamo <abianamo282@gmail.com>
2022-09-08 15:30:52 -0700

    third party authentications

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter')
 var config = require('./config');
 
 const mongoose = require('mongoose');
@@ -22,11 +24,11 @@ const Dishes = require('./models/dishes');
 var passport = require('passport');
 var authenticate = require('./authenticate');
 
-// Connection URL
+
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
     useMongoClient: true,
-    /* other options */
+    
   });
 
 connect.then((db) => {
@@ -34,6 +36,16 @@ connect.then((db) => {
 }, (err) => { console.log(err); });
 
 var app = express();
+
+
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -46,13 +58,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 
-// app.use(session({
-//   name: 'session-id',
-//   secret: '12345-67890-09876-54321',
-//   saveUninitialized: false,
-//   resave: false,
-//   store: new FileStore()
-// }));
 
 app.use(passport.initialize());
 // app.use(passport.session());
@@ -60,29 +65,17 @@ app.use(passport.initialize());
 app.use('/', index);
 app.use('/users', users);
 
-// function auth (req, res, next) {
-//   console.log(req.user);
 
-//   if (!req.user) {
-//     var err = new Error('You are not authenticated!');
-//     res.setHeader('WWW-Authenticate', 'Basic');                          
-//     err.status = 401;
-//     next(err);
-//   }
-//   else {
-//         next();
-//   }
-// }
-
-// app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
+app.use('/favorites',favoriteRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
--- routes/config.js
+++ routes/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey':'12345-67890-54321',
-    'mongoUrl':'mongo://localhost:27017/conFusion'
+    'mongoUrl':'mongo://localhost:27017/conFusion',
+    'facebook': {
+        clientId: 'Your Client App ID',
+        clientSecret: 'Your Client App Secret'
+    }
 }
